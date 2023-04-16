10280de9f1f69c06531bbe933f7f783c43038e53
gabrielyin <2021gabrielyin@gmail.com>
2023-04-16 14:45:39 -0300

    portfolio mvp done

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,33 @@
+import Link from "next/link";
+import { useState } from "react";
+import { AiOutlineInstagram, AiOutlineYoutube } from 'react-icons/ai';
+import { FaReact } from 'react-icons/fa';
+import { TbBrandNextjs } from 'react-icons/tb'
+import { SiTailwindcss } from 'react-icons/si';
+import loginImage from '../assets/pagina-de-login.png';
+import todoListImage from '../assets/todo-list.png';
 import Image from "next/image";
-import { useState } from "react"
+import { useRouter } from "next/router";
 
-export default function Home() {
+export
+  default function Home() {
   const [onActive, setOnActive] = useState('portfolio');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const router = useRouter()
+
+  function sendMessage(ev) {
+    ev.preventDefault();
+    if (!name || !message) {
+      alert('Alguns dados estão faltando')
+    } else {
+      const encodedName = encodeURI(name);
+      const encodedMessage = encodeURI(message);
+      window.open(`https://api.whatsapp.com/send?phone=5521983728281&text=(${encodedName})%20${encodedMessage}`, '_blank', 'noreferrer')
+      setMessage('');
+      setName('');
+    }
+  }
 
   return (
     <div className="min-h-screen bg-primary">
@@ -28,24 +53,136 @@ export default function Home() {
         </button>
       </div >
       {onActive === 'portfolio' ? (
-        <main className="max-w-4xl mx-auto px-4 md:px-0">
+        <main className="max-w-4xl mx-auto px-4 md:px-0 py-10 flex flex-col gap-4">
           <div
-            className="bg-secondary rounded-lg overflow-hidden"
+            className="bg-secondary rounded-lg overflow-hidden min-h-[600px] md:min-h-[300px] flex flex-col md:flex-row cursor-pointer"
           >
-            <img
-              src='https://images.unsplash.com/photo-1487014679447-9f8336841d58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1305&q=80'
-            />
             <div
-              className="p-2"
+              className="h-[300px] md:w-2/5"
             >
-              info
+              <Image
+                alt='login image'
+                src={loginImage}
+                className="object-cover h-full w-full"
+              />
+            </div>
+            <div
+              className="p-4 grow flex flex-col justify-between md:w-3/5"
+            >
+              <div>
+                <h2 className="text-3xl font-semibold mb-2 text-button">Página de Login</h2>
+                <p className="text-white">
+                  Nesse projeto o objetivo foi construir uma página de login e de criar conta para autenticar o usuário que é utilizado em várias aplicações web.
+                </p>
+              </div>
+              <div className="flex text-white justify-between text-3xl">
+                <div>
+                  <h2 className="text-lg mb-1">
+                    Tecnologias
+                  </h2>
+                  <div className="flex gap-2 ">
+                    <Link href='#'>
+                      <FaReact />
+                    </Link>
+                    <Link href='#'>
+                      <SiTailwindcss />
+                    </Link>
+                  </div>
+                </div>
+                <div className="flex items-end gap-2">
+                  <Link href='#'>
+                    <AiOutlineYoutube />
+                  </Link>
+                  <Link href='#'>
+                    <AiOutlineInstagram />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="bg-secondary rounded-lg overflow-hidden min-h-[600px] md:min-h-[300px] flex flex-col md:flex-row"
+          >
+            <div
+              className="h-[300px] md:w-2/5"
+            >
+              <Image
+                alt='todolistimage'
+                src={todoListImage}
+                className="object-cover h-full w-full"
+              />
+            </div>
+            <div
+              className="p-4 grow flex flex-col justify-between md:w-3/5"
+            >
+              <div>
+                <h2 className="text-3xl font-semibold mb-2 text-button">Todo List App</h2>
+                <p className="text-white">
+                  Nesse projeto o objetivo foi criar um todo list app onde o usuário pode criar e acompanhar as suas tarefas. Essa aplicação foi feita com o Nextjs e TailwindCSS.
+                </p>
+              </div>
+              <div className="flex text-white justify-between text-3xl">
+                <div>
+                  <h2 className="text-lg mb-1">
+                    Tecnologias
+                  </h2>
+                  <div className="flex gap-2 ">
+                    <Link href='#'>
+                      <FaReact />
+                    </Link>
+                    <Link href='#'>
+                      <SiTailwindcss />
+                    </Link>
+                    <Link href='#'>
+                      <TbBrandNextjs />
+                    </Link>
+                  </div>
+                </div>
+                <div className="flex items-end gap-2">
+                  <Link href='#'>
+                    <AiOutlineYoutube />
+                  </Link>
+                  <Link href='#'>
+                    <AiOutlineInstagram />
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </main>
       ) : (
-        <>
-          contato
-        </>
+        <form
+          onSubmit={(ev) => sendMessage(ev)}
+          className="max-w-xl bg-white rounded-lg p-4 border border-gray-300 flex flex-col gap-4 mx-3 md:mx-auto my-10"
+        >
+          <div>
+            <h4 className="font-semibold">Tem alguma dúvida? mande uma mensagem</h4>
+          </div>
+          <label className="flex flex-col">
+            Nome
+            <input
+              className="outline-blue-500 py-1.5 px-2 rounded-md border-2 border-gray-300 transition-all"
+              type="text"
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+            />
+          </label>
+          <label className="flex flex-col">
+            Mensagem
+            <textarea
+              className="outline-blue-500 py-1.5 px-2 rounded-md border-2 border-gray-300 min-h-[100px] transition-all"
+              type="text"
+              value={message}
+              onChange={(ev) => setMessage(ev.target.value)}
+              placeholder="Mande sua mensagem aqui..."
+            />
+          </label>
+          <input
+            type="submit"
+            className="cursor-pointer bg-green-500 py-2 px-3 rounded-md text-white font-semibold tracking-wide hover:bg-green-600 transition-all w-fit mt-4 outline-none focus:ring-2 ring-blue-500"
+            value='Enviar WhatsApp'
+          />
+        </form>
       )}
     </div >
   )
